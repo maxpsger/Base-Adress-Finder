7426e02c7021abca520b2c940c06195d48e1b4b9
maxpsger <maximilian@schmidt-altdorf.de>
2025-07-08 20:05:41 +0200

    Add files via upload

---
+++ read memory/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <windows.h>
+#include <tlhelp32.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+uintptr_t GetModuleBaseAddress(DWORD procId) {
+    uintptr_t modBaseAddr = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+    if (hSnap != INVALID_HANDLE_VALUE) {
+        MODULEENTRY32W modEntry;
+        modEntry.dwSize = sizeof(modEntry);
+        if (Module32FirstW(hSnap, &modEntry)) {
+            // Das erste Modul ist i.d.R. das Main-Module
+            modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+        }
+        CloseHandle(hSnap);
+    }
+    return modBaseAddr;
+}
+
+int main() {
+    DWORD pid;
+    std::cout << "Enter PID: ";
+    std::cin >> pid;
+    std::cin.ignore(); // Leert Eingabepuffer
+
+    uintptr_t base = GetModuleBaseAddress(pid);
+    if (base == 0) {
+        std::cout << "Base address not found!\n";
+        std::cout << "Press Enter to exit...";
+        std::cin.get();
+        return 1;
+    }
+
+    std::ofstream out("base_address.txt");
+    out << std::hex << base;
+    out.close();
+    std::cout << "Base address written to base_address.txt: 0x" << std::hex << base << std::endl;
+
+    std::cout << "Done. Press Enter to exit...";
+    std::cin.get();
+    return 0;
+}
